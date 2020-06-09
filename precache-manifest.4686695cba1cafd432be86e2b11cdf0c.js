d5741810d7367ba302b9b912b0ce19eb2e53d7df
frescobar <frescobar87@gmail.com>
2020-06-09 16:22:20 -0500

    Updates

---
+++ precache-manifest.4686695cba1cafd432be86e2b11cdf0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e97397674692477f74313ad6a26722b",
+    "url": "/monster-app/index.html"
+  },
+  {
+    "revision": "4cabadd98907b2fa1bc0",
+    "url": "/monster-app/static/css/main.9dcb4958.chunk.css"
+  },
+  {
+    "revision": "cff66eaf6bd606d518f6",
+    "url": "/monster-app/static/js/2.4b6796e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-app/static/js/2.4b6796e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cabadd98907b2fa1bc0",
+    "url": "/monster-app/static/js/main.a1b43d2d.chunk.js"
+  },
+  {
+    "revision": "41027d0c2139f2d494ba",
+    "url": "/monster-app/static/js/runtime-main.3a0c5eb6.js"
+  }
+]);
