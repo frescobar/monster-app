b405b0d69df1bcccee6a3b2167aacd4f0c6d5e76
frescobar <frescobar87@gmail.com>
2020-06-17 20:25:43 -0500

    Change the size of the card

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import "./Card.css"
+import React from "react";
+import "./Card.css";
 
-const Card = ({monster}) => {
-    return (
-      
-        <div className ="card-container">
-            <img src= {`https://robohash.org/${monster.id}?set=set2&size=160x200`} alt ={monster.name}/>
-            <h2>{monster.name}</h2>
-            <p>{monster.email}</p>
-        </div>
-        
-    );
-}
+const Card = ({ monster }) => {
+  return (
+    <div className="card-container">
+      <img
+        src={`https://robohash.org/${monster.id}?set=set2&size=180x200`}
+        alt={monster.name}
+      />
+      <h2>{monster.name}</h2>
+      <p>{monster.email}</p>
+    </div>
+  );
+};
 
 export default Card;
